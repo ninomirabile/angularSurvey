584dc0e108b1c77bb89e4155a54f8493720f95b7
ninomirabile <ninomirabile@hotmail.it>
2025-07-29 08:42:08 +0200

    Implement Survey Builder module with editor

---
+++ survey-app/src/app/features/survey-builder/survey-builder.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { SurveyBuilderComponent } from './survey-builder.component';
+
+export const SURVEY_BUILDER_ROUTES: Routes = [
+  {
+    path: '',
+    component: SurveyBuilderComponent,
+    title: 'Survey Builder'
+  }
+]; 
